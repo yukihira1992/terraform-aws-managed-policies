a91174fc0b7f15cc0632d2874744c266f048447e
yukihira1992 <ykhr0130@gmail.com>
2020-06-18 19:04:17 +0000

    Updated policies and bumped version 0.1.38.

--- outputs.tf
+++ outputs.tf
@@ -2506,6 +2506,10 @@ output "AWSQuicksightAthenaAccess" {
   value = "arn:aws:iam::aws:policy/service-role/AWSQuicksightAthenaAccess"
 }
 
+output "AWSServiceRoleForThorInternalDevPolicy" {
+  value = "arn:aws:iam::aws:policy/service-role/AWSServiceRoleForThorInternalDevPolicy"
+}
+
 output "AWSShieldDRTAccessPolicy" {
   value = "arn:aws:iam::aws:policy/service-role/AWSShieldDRTAccessPolicy"
 }
