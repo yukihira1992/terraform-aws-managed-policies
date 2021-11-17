e12c834aed0b7518673809c56eaf4c44e4c7e7f4
Takayuki Hirayama <hirayama@atama.plus>
2021-11-17 23:02:26 +0900

    fix version updator

--- create-patch.sh
+++ create-patch.sh
@@ -4,7 +4,8 @@ if [[ -n "$(git status --porcelain)" ]]; then
     git remote set-url origin https://yukihira1992:${GITHUB_TOKEN}@github.com/yukihira1992/terraform-aws-managed-policies.git
     git config --local user.name "yukihira1992"
     git config --local user.email "ykhr0130@gmail.com"
-    NEW_VERSION=`cat version.txt | python next_patch.py`
+    CURRENT_VERSION=`cat version.txt`
+    NEW_VERSION=`python next_patch.py $CURRENT_VERSION`
     if [[ -z "$NEW_VERSION" ]]; then
         echo "Failed to bump version."
         exit 1
